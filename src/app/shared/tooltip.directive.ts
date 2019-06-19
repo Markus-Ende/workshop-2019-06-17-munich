2a3a966d3544de5de8410fc08663975592886144
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-06-19 21:31:57 +0200

    tooltip 2

--- src/app/shared/tooltip.directive.ts
+++ src/app/shared/tooltip.directive.ts
@@ -1,17 +1,33 @@
-import { Directive, Input, HostListener } from '@angular/core';
+import {
+  Directive,
+  Input,
+  HostListener,
+  ElementRef,
+  Inject
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Directive({
   selector: '[tooltip]'
 })
 export class TooltipDirective {
-  @Input() tooltip = 'default tooltip';
+  @Input() set tooltipText(newVal: string) {
+    this.tooltipElement.innerText = newVal;
+  }
+
+  private tooltipElement = this.doc.createElement('div');
 
   @HostListener('mouseenter') onMouseEnter() {
     console.log('tooltip entered');
+    this.elementRef.nativeElement.appendChild(this.tooltipElement);
   }
   @HostListener('mouseleave') onMouseLeave() {
     console.log('tooltip left');
+    this.elementRef.nativeElement.removeChild(this.tooltipElement);
   }
 
-  constructor() {}
+  constructor(
+    private elementRef: ElementRef,
+    @Inject(DOCUMENT) private doc: Document
+  ) {}
 }
